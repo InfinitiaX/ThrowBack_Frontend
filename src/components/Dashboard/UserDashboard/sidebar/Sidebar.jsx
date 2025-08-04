661672d464c84669bb1072348a356cc4556e7694
InfinitiaX <contact@infinitiax.com>
2025-08-04 17:12:15 +0200

    New37 commit

--- src/components/Dashboard/UserDashboard/sidebar/Sidebar.jsx
+++ src/components/Dashboard/UserDashboard/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import styles from './Sidebar.module.css';
-import Logo from '../../../../../public/Logo.png';
+import Logo from '../../../../../public/images/Logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faVideo,

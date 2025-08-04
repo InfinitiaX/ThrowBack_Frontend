edce9b7d61d25e7e44755c6aa106bd53927f7bd4
InfinitiaX <contact@infinitiax.com>
2025-08-04 17:00:50 +0200

    New35 commit

---
+++ src/components/Dashboard/UserDashboard/Home.jsx
@@ -0,0 +1,16 @@
+// src/components/Dashboard/UserDashboard/Home.jsx
+import React from 'react';
+import styles from './styles.module.css';
+
+const Home = () => (
+  <main className={styles.main_content}>
+    <div className={styles.welcome_section}>
+      <h1 className={styles.friendly_message}>In development</h1>
+      <p className={styles.playlist_title}>This module is still being implemented</p>
+    </div>
+    
+    
+  </main>
+);
+
+export default Home;

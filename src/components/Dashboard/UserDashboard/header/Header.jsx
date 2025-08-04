8c1becaaf92dd137c4a0c419d74d50c5243f875c
InfinitiaX <contact@infinitiax.com>
2025-08-04 17:19:21 +0200

    New38 commit

--- src/components/Dashboard/UserDashboard/header/Header.jsx
+++ src/components/Dashboard/UserDashboard/header/Header.jsx
@@ -22,7 +22,7 @@ import {
   faUsers,
   faFilm
 } from '@fortawesome/free-solid-svg-icons';
-import Logo from '../../../../../public/images/Logo.png';
+import Logo from '../../../../images/Logo.png';
 import { useAuth } from '../../../../contexts/AuthContext';
 // Importer le service de recherche
 import { searchAPI } from '../../../../utils/api';
